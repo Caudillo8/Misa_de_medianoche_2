dc08d7bcfb6d799b42f36015a08740d72c411295
hernandez matias <40743114+Caudillo8@users.noreply.github.com>
2022-10-17 10:01:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function mostarMenu(){
+    document.getElementById('menu0').style.display='block';
+    document.getElementById('menu1').style.display='block';
+    document.getElementById('menu2').style.display='block';
+    document.getElementById('menu3').style.display='block';
+    document.getElementById('menu4').style.display='block';
+    document.getElementById('menu5').style.display='block';
+   
+}
+/*
+function mostarMenu(){
+    console.log("apretado");
+    const menu0 = document.getElementById('menu0');
+    const menu1 = document.getElementById('menu1');
+    const menu2 = document.getElementById('menu2');
+    const menu3 = document.getElementById('menu3');
+    const menu4 = document.getElementById('menu4');
+    const menu5 = document.getElementById('menu5');
+    if( menu0.classList.contains='menu'){
+        menu0.classList.add = 'visible';
+    }else{
+        menu0.classList.remove = 'visible';
+    }
+}*/
